e29df3a09a954cf35a96138e6c623f5f778b6903
SIQVerse <smlzulu@gmail.com>
2025-10-20 12:35:55 -0700

    Create models.py

---
+++ apps/siqposts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+
+class CivicPost(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
