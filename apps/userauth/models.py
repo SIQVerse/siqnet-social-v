eaa00e6802ad11ee43c668f0f44f3e70b3df08e6
SIQVerse <smlzulu@gmail.com>
2025-10-20 14:25:59 -0700

    Update models.py

--- apps/userauth/models.py
+++ apps/userauth/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
     location = models.CharField(max_length=100, blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
 
     def __str__(self):
         return self.username
