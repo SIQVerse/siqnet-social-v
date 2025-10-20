ec807e0c3fed3710abc19029e81d54d8cb8b74c2
SIQVerse <smlzulu@gmail.com>
2025-10-20 15:35:36 -0700

    Update urls.py

--- apps/siqposts/urls.py
+++ apps/siqposts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:post_id>/', views.post_detail_view, name='post_detail'),
     path('create/', views.post_create_view, name='post_create'),
     path('<int:post_id>/edit/', views.post_edit_view, name='post_edit'),
+    path('<int:post_id>/delete/', views.post_delete_view, name='post_delete'),
 ]
