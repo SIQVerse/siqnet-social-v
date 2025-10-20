746924db54fbd07e0014839ee00653e23e5747e3
SIQVerse <smlzulu@gmail.com>
2025-10-20 14:29:20 -0700

    Update views.py

--- apps/userauth/views.py
+++ apps/userauth/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm, UserLoginForm
-from .models import CustomUser
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
+        form = UserRegistrationForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
             login(request, user)
@@ -17,14 +16,10 @@ def register_view(request):
 
 def login_view(request):
     if request.method == 'POST':
-        form = UserLoginForm(request.POST)
+        form = UserLoginForm(request, data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user:
-                login(request, user)
-                return redirect('profile')
+            login(request, form.get_user())
+            return redirect('profile')
     else:
         form = UserLoginForm()
     return render(request, 'userauth/login.html', {'form': form})
