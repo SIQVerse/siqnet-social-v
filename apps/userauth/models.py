cd3efa4610c4711b3ac82bd578ff03c95ff66fa7
SIQVerse <smlzulu@gmail.com>
2025-10-21 18:00:09 -0700

    Update models.py

--- apps/userauth/models.py
+++ apps/userauth/models.py
@@ -6,5 +6,16 @@ class CustomUser(AbstractUser):
     location = models.CharField(max_length=100, blank=True)
     profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
 
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        blank=True
+    )
+
     def __str__(self):
         return self.username
