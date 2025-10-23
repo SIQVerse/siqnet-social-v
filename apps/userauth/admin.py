6543274236c160440dcd61c2bf4c7cb5df560c31
SIQVerse <smlzulu@gmail.com>
2025-10-23 11:31:38 -0700

    Update admin.py

--- apps/userauth/admin.py
+++ apps/userauth/admin.py
@@ -4,11 +4,66 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
+
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('bio', 'location', 'profile_picture')}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('bio', 'location', 'profile_picture')}),
+        ("Profile Info", {
+            'fields': (
+                'bio',
+                'location',
+                'profile_picture',
+                'badge',
+                'is_verified',
+                'reputation_score',
+                'last_seen',
+                'is_online',
+            )
+        }),
+        ("Social & Privacy", {
+            'fields': (
+                'followers',
+                'allow_messages',
+                'allow_tags',
+                'role',
+            )
+        }),
     )
 
-admin.site.register(CustomUser, CustomUserAdmin)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Profile Info", {
+            'fields': (
+                'bio',
+                'location',
+                'profile_picture',
+                'badge',
+                'is_verified',
+                'reputation_score',
+                'last_seen',
+                'is_online',
+            )
+        }),
+        ("Social & Privacy", {
+            'fields': (
+                'followers',
+                'allow_messages',
+                'allow_tags',
+                'role',
+            )
+        }),
+    )
+
+    list_display = (
+        'username',
+        'email',
+        'role',
+        'is_verified',
+        'reputation_score',
+        'is_online',
+        'last_login',
+        'date_joined',
+    )
+    search_fields = (
+        'username',
+        'email',
+        'location',
+        'badge',
+    )
