8f0380b80fa626d3943c4877b37fb7a2e2c63410
SIQVerse <smlzulu@gmail.com>
2025-10-23 11:41:48 -0700

    Update asgi.py

--- siqnet_backend/asgi.py
+++ siqnet_backend/asgi.py
@@ -1,6 +1,8 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "siqnet_backend.settings")
+django.setup()
 
-application = get_asgi_application()
+application = get_default_application()
