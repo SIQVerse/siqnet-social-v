dc0d638a1742a0dece277fd0ec4670bb758c7b20
SIQVerse <smlzulu@gmail.com>
2025-10-25 06:31:12 -0700

    Update apps.py

--- apps/siqposts/apps.py
+++ apps/siqposts/apps.py
@@ -4,4 +4,6 @@ from django.apps import AppConfig
 class SiqPostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.siqposts'
+
+    # Optional: explicitly set the app path if needed
     path = os.path.abspath(os.path.dirname(__file__))
