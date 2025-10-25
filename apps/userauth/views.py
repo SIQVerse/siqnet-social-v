eaff744e76d6e7b0a4cf893b341dad8355bd035f
SIQVerse <smlzulu@gmail.com>
2025-10-24 17:39:58 -0700

    Update views.py

--- apps/userauth/views.py
+++ apps/userauth/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
-from .models import CivicPost  # Assuming CivicPost is defined in models.py
+from django.shortcuts import render, get_object_or_404
+from .models import CivicPost
 
 User = get_user_model()
 
 def profile_view(request, username):
-    user = User.objects.get(username=username)
-    return render(request, 'userauth/profile.html', {'user': user})
+    user = get_object_or_404(User, username=username)
+    posts = CivicPost.objects.filter(author=user).order_by('-created_at')
+    return render(request, 'userauth/profile.html', {
+        'user': user,
+        'posts': posts
+    })
