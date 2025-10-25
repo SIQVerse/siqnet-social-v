9b568de77890a96145c3046ef062a039574cd200
SIQVerse <smlzulu@gmail.com>
2025-10-25 06:37:06 -0700

    Update asgi.py

--- siqnet_backend/asgi.py
+++ siqnet_backend/asgi.py
@@ -2,7 +2,11 @@ import os
 import django
 from channels.routing import get_default_application
 
+# Set the default settings module for Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "siqnet_backend.settings")
+
+# Setup Django
 django.setup()
 
+# Get the default ASGI application (includes WebSocket routing)
 application = get_default_application()
